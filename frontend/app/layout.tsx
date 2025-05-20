b261bc67d324c79457554d8115ffd20b71353dbd
juniorteixeiradev <teixeirajrwd@gmail.com>
2025-05-20 13:30:18 -0300

    config: deploy && build

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
+import './globals.css';
+import { ToastContainer } from 'react-toastify';
+import { ThemeProvider } from '@/components/theme-provider';
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Alfa Manager - Alfa Tecnologia',
+  description: 'Software de gestão de vendas desenvolvido por alfa tecnologia',
+};
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ToastContainer />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}

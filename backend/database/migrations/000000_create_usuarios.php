de1331039901695ecaf0c56fb8fc14d3431d354e
Junior Teixeira <122942785+juniorteixeiradev@users.noreply.github.com>
2025-06-21 14:11:56 -0300

    Update 000000_create_usuarios.php

--- backend/database/migrations/000000_create_usuarios.php
+++ backend/database/migrations/000000_create_usuarios.php
@@ -11,27 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
-            $table->uuid('uuid');
-            $table->uuid('empresa_id')->nullable();
-            $table->string('nome');
-            $table->string('sobrenome');
-            $table->string('cpf')->unique();
-            $table->string('telefone')->unique();
-            $table->string('endereco');
-            $table->string('cidade');
-            $table->string('estado');
-            $table->string('cep');
-            $table->string('email')->unique();
-            $table->string('senha');
-            $table->boolean('ativo')->default(true);
-            $table->timestamps();
-            $table->rememberToken();
-            $table->softDeletes();
+        Schema::dropIfExists('usuarios');
+        // Schema::create('usuarios', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->uuid('uuid');
+        //     $table->uuid('empresa_id')->nullable();
+        //     $table->string('nome');
+        //     $table->string('sobrenome');
+        //     $table->string('cpf')->unique();
+        //     $table->string('telefone')->unique();
+        //     $table->string('endereco');
+        //     $table->string('cidade');
+        //     $table->string('estado');
+        //     $table->string('cep');
+        //     $table->string('email')->unique();
+        //     $table->string('senha');
+        //     $table->boolean('ativo')->default(true);
+        //     $table->timestamps();
+        //     $table->rememberToken();
+        //     $table->softDeletes();
         
-            $table->foreign('empresa_id')->references('id')->on('empresas')->onDelete('cascade');
-        });
+        //     $table->foreign('empresa_id')->references('id')->on('empresas')->onDelete('cascade');
+        // });
     }
 
     /**
@@ -39,6 +40,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('usuarios');
+       //
     }
 };

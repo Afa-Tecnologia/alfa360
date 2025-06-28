e96601f8e22e3619b6259a5c5f9fc023546f2f78
juniorteixeiradev <teixeirajrwd@gmail.com>
2025-06-28 11:23:34 -0300

    fix: /me nao retornava a role do usuario

--- backend/app/Http/Resources/UserResource.php
+++ backend/app/Http/Resources/UserResource.php
@@ -2,23 +2,26 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'uuid' => $this->uuid,
-            'name' => $this->name,
-            'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
-            'created_at' => $this->created_at->format('d-m-Y H:i:s'),
-            'updated_at' => $this->updated_at->format('d-m-Y H:i:s'),
-            'deleted_at' => $this->deleted_at,
-            'tenant_id' => $this->tenant_id,
-            'role' => $this->getRoleNames()->first(),
-        ];
+        // return [
+        //     'id' => $this->id,
+        //     'uuid' => $this->uuid,
+        //     'name' => $this->name,
+        //     'email' => $this->email,
+        //     'email_verified_at' => $this->email_verified_at,
+        //     'created_at' => $this->created_at->format('d-m-Y H:i:s'),
+        //     'updated_at' => $this->updated_at->format('d-m-Y H:i:s'),
+        //     'deleted_at' => $this->deleted_at,
+        //     'tenant_id' => $this->tenant_id,
+        //     'role' => $this->getRoleNames()->first(),
+        // ];
+
+        return User::where('id', $this->id)->first();
     }
 }
